ebf84aee575f1aadbac644741fc3896931beaa36
Samuel Wilcox <samuelwilcox@outlook.com>
2023-02-27 10:04:36 +0000

    add variant field to stickerkit object

--- csgo/stickerkits.go
+++ csgo/stickerkits.go
@@ -2,7 +2,41 @@ package csgo
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/pkg/errors"
+)
+
+type stickerVariant string
+
+const (
+	stickerVariantPaper      stickerVariant = "Paper"
+	stickerVariantGlossy     stickerVariant = "Glossy"
+	stickerVariantGlitter    stickerVariant = "Glitter"
+	stickerVariantHolo       stickerVariant = "Holo"
+	stickerVariantFoil       stickerVariant = "Foil"
+	stickerVariantGold       stickerVariant = "Gold"
+	stickerVariantLenticular stickerVariant = "Lenticular"
+)
+
+var (
+	stickerVariantIdSuffixes = map[string]stickerVariant{
+		"_paper":      stickerVariantPaper,
+		"_glossy":     stickerVariantGlossy,
+		"_glitter":    stickerVariantGlitter,
+		"_holo":       stickerVariantHolo,
+		"_foil":       stickerVariantFoil,
+		"_gold":       stickerVariantGold,
+		"_lenticular": stickerVariantLenticular,
+	}
+
+	stickerVariantNameSuffixes = map[string]stickerVariant{
+		"(Glitter)":    stickerVariantGlitter,
+		"(Holo)":       stickerVariantHolo,
+		"(Foil)":       stickerVariantFoil,
+		"(Gold)":       stickerVariantGold,
+		"(Lenticular)": stickerVariantLenticular,
+	}
 )
 
 // Stickerkit represents a Stickerkit object from the items_game file.
@@ -11,12 +45,15 @@ type Stickerkit struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	RarityId    string `json:"rarityId"`
+	Variant     string `json:"variant"`
 }
 
 // mapToStickerkit converts the provided data map into a Stickerkit object.
 func mapToStickerkit(data map[string]interface{}, language *language) (*Stickerkit, error) {
 
-	response := &Stickerkit{}
+	response := &Stickerkit{
+		Variant: "Paper",
+	}
 
 	// get Name
 	if val, err := crawlToType[string](data, "name"); err != nil {
@@ -25,7 +62,16 @@ func mapToStickerkit(data map[string]interface{}, language *language) (*Stickerk
 		response.Id = val
 	}
 
-	// get language Name Id
+	// identify sticker variant from id
+	for suffix, variant := range stickerVariantIdSuffixes {
+		if !strings.HasSuffix(response.Id, suffix) {
+			continue
+		}
+
+		response.Variant = string(variant)
+	}
+
+	// get language Name
 	if val, err := crawlToType[string](data, "item_name"); err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("item_name missing from Stickerkit (%s)", response.Id))
 	} else {
@@ -38,7 +84,16 @@ func mapToStickerkit(data map[string]interface{}, language *language) (*Stickerk
 		response.Name = lang
 	}
 
-	// get language Description Id
+	// identify sticker variant from descriptive name (which takes precedence)
+	for suffix, variant := range stickerVariantNameSuffixes {
+		if !strings.HasSuffix(response.Name, suffix) {
+			continue
+		}
+
+		response.Variant = string(variant)
+	}
+
+	// get language Description
 	if val, err := crawlToType[string](data, "description_string"); err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("description_string missing from Stickerkit (%s)", response.Id))
 	} else {
